94a9333a848639d18980e321369ea37d3bcbbb0c
Mka91 <kiwogens@yandex.ru>
2022-03-14 21:37:34 +0700

    Add project files.

---
+++ Range/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Range range = new Range(3, 7);
+
+            Console.WriteLine("Длина диапазона: " + range.getLength());
+            Console.WriteLine("Наличие числа в диапазоне: " + range.isInside(2));
+            Console.WriteLine("Наличие числа в диапазоне: " + range.isInside(5.5));
+        }
+    }
+}
---
+++ Range/Range.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Range
+{
+    internal class Range
+    {
+        public double from
+        {
+            get;
+            set;
+        }
+        public double to
+        {
+            get;
+            private set;
+        }
+
+        public Range(double from, double to)
+        {
+            this.from = from;
+            this.to = to;
+        }
+
+        public double getLength()
+        {
+            return from + to;
+        }
+
+        public bool isInside(double number)
+        {
+            return from <= number && to >= number;
+        }
+    }
+}
