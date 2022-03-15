fe05c7db0868eac7d6c2250adaee7cd958b6c5bf
Mka91 <kiwogens@yandex.ru>
2022-03-15 15:29:44 +0700

    Range

--- Range/Program.cs
+++ Range/Program.cs
@@ -8,13 +8,30 @@ namespace Range
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Range range = new Range(3, 7);
+            var range = new Range(3, 9);
 
-            Console.WriteLine("Длина диапазона: " + range.getLength());
-            Console.WriteLine("Наличие числа в диапазоне: " + range.isInside(2));
-            Console.WriteLine("Наличие числа в диапазоне: " + range.isInside(5.5));
+            Console.WriteLine("Длина диапазона: " + range.GetLength);
+            Console.WriteLine("Наличие числа в диапазоне: " + range.IsInside(2));
+            Console.WriteLine("Наличие числа в диапазоне: " + range.IsInside(7));
+
+            var intersection = range.GetIntersection(new Range(3, 8));
+            Console.WriteLine("Пересечение диапазонов:" + intersection);
+
+            Range[] union = range.GetUnion(new Range(1, 4));
+            Console.WriteLine("Объединение диапазонов:");
+            foreach (Range r in union)
+            {
+                Console.WriteLine(r.ToString());
+            }
+
+            Range[] difference = range.GetDifference(new Range(4, 6));
+            Console.WriteLine("Разность 2 интервалов: ");
+            foreach (Range r in difference)
+            {
+                Console.WriteLine(r.ToString()) ;
+            }
         }
     }
 }
--- Range/Range.cs
+++ Range/Range.cs
@@ -8,31 +8,81 @@ namespace Range
 {
     internal class Range
     {
-        public double from
+        public Range(double from, double to)
+        {
+            this.From = from;
+            this.To = to;
+        }
+        public double From
         {
             get;
             set;
         }
-        public double to
+        public double To
         {
             get;
             private set;
         }
-
-        public Range(double from, double to)
+        public double GetLength
         {
-            this.from = from;
-            this.to = to;
+            get
+            {
+                return To - From;
+            }
         }
 
-        public double getLength()
+        public bool IsInside(double number)
         {
-            return from + to;
+            return (number - From > double.Epsilon && To - number > double.Epsilon) || (Math.Abs(From - number) <= double.Epsilon) || (Math.Abs(To - number) <= double.Epsilon);
         }
 
-        public bool isInside(double number)
+        public Range GetIntersection(Range range)
         {
-            return from <= number && to >= number;
+            if (range.From - To > double.Epsilon || Math.Abs(range.From - To) <= double.Epsilon || From - range.To > double.Epsilon || Math.Abs(From - range.To) <= double.Epsilon)
+            {
+                return null;
+            }
+
+            return new Range(Math.Max(From, range.From), Math.Min(To, range.To));
+        }
+        public Range[] GetUnion(Range range)
+        {
+            if (range.From - To > double.Epsilon || From - range.To > double.Epsilon)
+            {
+                return new Range[] { new Range(From, To), new Range(range.From, range.To) };
+            }
+
+            return new Range[] { new Range(Math.Min(From, range.From), Math.Max(To, range.To)) };
+        }
+
+        public Range[] GetDifference(Range range)
+        {
+            if ( range.From - From > double.Epsilon && To - range.To > double.Epsilon)
+            {
+                return new Range[] { new Range(From, range.From), new Range(range.To, To) };
+            }
+
+            if (range.From - From > double.Epsilon && To - range.From > double.Epsilon && (range.To - To > double.Epsilon || Math.Abs(range.To - To) <= double.Epsilon))
+            {
+                return new Range[] { new Range(From, range.From) };
+            }
+
+            if ((range.From - From> double.Epsilon || Math.Abs(From - range.From) <= double.Epsilon && To - range.To > double.Epsilon && range.To - From > double.Epsilon))
+            {
+                return new Range[] { new Range(range.To, To) };
+            }
+
+            if ((From - range.From > double.Epsilon || Math.Abs(From - range.From) <= double.Epsilon && (range.To - To > double.Epsilon || Math.Abs(range.To - To) <= double.Epsilon)))
+            {
+                return new Range[0];
+            }
+
+            return new Range[] { new Range(From, To) };
+        }
+
+        public override string ToString()
+        {
+            return $"from: {From}, to: {To}";
         }
     }
 }
