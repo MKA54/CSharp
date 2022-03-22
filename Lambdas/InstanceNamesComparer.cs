5f954f91b8f51231e463f7bcb66be5f0edf6d355
Mka91 <kiwogens@yandex.ru>
2022-03-22 17:07:45 +0700

    Lambda

---
+++ Lambdas/InstanceNamesComparer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambdas
+{
+    internal class InstanceNamesComparer : EqualityComparer<Person>
+    {
+        public override bool Equals(Person p1, Person p2)
+        {
+            if (Object.ReferenceEquals(p1, p2))
+            {
+                return true;
+            }
+
+            if (Object.ReferenceEquals(p1, null) || Object.ReferenceEquals(p2, null))
+            {
+                return false;
+            }
+
+            return Equals(p1.Name, p2.Name);
+        }
+
+        public override int GetHashCode(Person p)
+        {
+            if (Object.ReferenceEquals(p, null))
+            {
+                return 0;
+            }
+
+            if(p.Name == null)
+            {
+                return 0;
+            }
+
+            var prime = 37;
+            var hash = 1; 
+
+            return prime * hash + p.Name.GetHashCode();
+        }
+    }
+}
---
+++ Lambdas/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambdas
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var list = new List<Person>
+            {
+                new Person("Роман", 12),
+                new Person("Игнат", 25),
+                new Person("Ольга", 38),
+                new Person("Мария", 25),
+                new Person("Валентина", 41),
+                new Person("Лазарь", 34),
+                new Person("Николай", 12),
+                new Person("Мария", 35),
+                new Person("Роман", 41),
+                new Person("Галина", 56),
+            };
+
+            var uniqueList = list.Distinct(new InstanceNamesComparer());
+
+            foreach (var item in uniqueList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
