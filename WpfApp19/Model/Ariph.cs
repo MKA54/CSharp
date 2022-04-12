7fbc6dd34feba4db60fb748d9e7acc1df4d3628b
Mka91 <kiwogens@yandex.ru>
2022-04-12 21:44:37 +0700

    Лабораторная работа нр.19

---
+++ WpfApp19/Model/Ariph.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace WpfApp19.Model
+{
+    static class Ariph
+    {
+        public static double Circumference(double radius) => 2 * Math.PI * radius;
+    }
+}
---
+++ WpfApp19/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using WpfApp19.Model;
+
+namespace WpfApp19.ViewModel
+{
+    internal class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecute);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private double _radius;
+        private double _result;
+        public ICommand AddCommand { get; }
+
+        public double Radius
+        {
+            get => _radius;
+            set
+            {
+                _radius = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Result
+        {
+            get => _result;
+            set
+            {
+                _result = value;
+                OnPropertyChanged();
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void OnAddCommandExecute(object parameter)
+        {
+            Result = Ariph.Circumference(Radius);
+        }
+
+        private bool CanAddCommandExecute(object parameter)
+        {
+            return Radius > 0;
+        }
+    }
+}
