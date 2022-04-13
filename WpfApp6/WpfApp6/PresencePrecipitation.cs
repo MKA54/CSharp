817af0a5b28c51a2ec52446323bbe9e1779d4526
Mka91 <kiwogens@yandex.ru>
2022-04-13 10:39:35 +0700

    Update

---
+++ WpfApp6/WpfApp6/PresencePrecipitation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp6
+{
+    internal enum PresencePrecipitation
+    {
+        Sunny = 0,
+        Cloudy = 1,
+        Rain = 2,
+        Snow = 3
+    }
+}
